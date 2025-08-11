e1b8c44b3c68ae8ce242e05b785e2e95d20c49ec
JRyan-123 <acostajoseryan4@gmail.com>
2025-08-11 21:05:43 +0800

    v1

--- bootstrap_02/assets/js/loader.js
+++ bootstrap_02/assets/js/loader.js
@@ -1,4 +1,4 @@
-function loadContent(id, file, fn) {
+function loadContent(id, file) {
 
 	fetch(file)
 		.then(response => response.text())
@@ -6,7 +6,16 @@ function loadContent(id, file, fn) {
 		.then(html => {
 			document.getElementById(id).innerHTML = html;
 
-			if (typeof fn === "function") fn();
+			// if (typeof fn === "function") fn();
+
+			
+			// const scripts = document.getElementById(id).getElementsByTagName('script');
+            //     	for (let script of scripts) {
+            //         const newScript = document.createElement('script');
+            //         newScript.src = script.src; // Load external scripts
+            //         newScript.text = script.innerHTML; // Execute inline scripts
+            //         document.body.appendChild(newScript);
+            //     }
 		})
 		.catch(error => console.error('Error: ', error))
 
@@ -15,3 +24,6 @@ loadContent('nav', 'partials/navbar.html');
 loadContent('hero', 'partials/hero.html');
 loadContent('rides', 'partials/rides.html');
 loadContent('promo', 'partials/promo.html');
+
+loadContent('contacts', 'partials/contacts.html');
+loadContent('footer', 'partials/footer.html');
